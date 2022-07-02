727ab55cfa60374644878bea71bc8df94c2b3075
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-02 15:51:45 +0530

    receipee added

---
+++ src/app/addreceipee/addreceipee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addreceipee',
+  templateUrl: './addreceipee.component.html',
+  styleUrls: ['./addreceipee.component.css']
+})
+export class AddreceipeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
